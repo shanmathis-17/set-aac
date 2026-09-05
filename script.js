734b331c0600c8505cf43659616895956255b9e3
shanmathis-17 <shanmathis463@gmail.com>
2026-09-05 17:14:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function makeRequest(request) {
     alert("Request: " + request);
+
+    const speech = new SpeechSynthesisUtterance(request);
+    speech.rate = 0.9;
+    speech.pitch = 1;
+
+    window.speechSynthesis.speak(speech);
 }
